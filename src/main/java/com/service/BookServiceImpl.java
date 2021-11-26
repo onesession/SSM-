8573c55e8e82061e9f6c64ea2589d4563b4ce5e5
“onesession” <274207784@qq.com>
2021-11-26 13:22:59 +0800

    SSM框架整合

---
+++ src/main/java/com/service/BookServiceImpl.java
@@ -0,0 +1,38 @@
+package com.service;
+
+import com.dao.BookMapper;
+import com.pojo.Books;
+
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+
+    //service调用dao层的操作，设置一个set接口，方便Spring管理
+    //@Autowired
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
